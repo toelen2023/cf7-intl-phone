b5d2724fb9696f553ae905b5157d75ed376fd46e
html-plus <html.plus.in.ua@gmail.com>
2026-07-19 19:44:16 +0300

    Add plugin bootstrap

---
+++ cf7-intl-phone.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Plugin Name: CF7 International Phone
+ * Plugin URI: https://github.com/toelen2023/cf7-intl-phone
+ * Description: International phone field for Contact Form 7.
+ * Version: 0.1.0
+ * Author: Olena
+ * Text Domain: cf7-intl-phone
+ * Domain Path: /languages
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+ exit;
+}
+
+define( 'CF7IP_VERSION', '0.1.0' );
+
+define( 'CF7IP_FILE', FILE );
+
+define( 'CF7IP_PATH', plugin_dir_path( FILE ) );
+
+define( 'CF7IP_URL', plugin_dir_url( FILE ) );
+
+
+require_once CF7IP_PATH . 'includes/class-plugin.php';
+
+$plugin = new CF7_Intl_Phone();
+
+$plugin->run();
